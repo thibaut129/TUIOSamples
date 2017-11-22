62894105902a5df03d62128008e9d40f715d91f3
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-11-22 14:41:55 +0100

    Debug for example health

--- src/menu.js
+++ src/menu.js
@@ -9,6 +9,40 @@ import $ from 'jquery/dist/jquery.min';
 import ImageElementWidget from 'tuiomanager/widgets/ElementWidget/ImageElementWidget/ImageElementWidget';
 import VideoElementWidget from 'tuiomanager/widgets/ElementWidget/VideoElementWidget/VideoElementWidget';
 
+
+function buildHealth() {
+  $('#app').empty();
+
+  $('#app').append('<div id="healthy-container">  <h1>Healthy</h1>  </div>');
+  $('#app').append('<div id="unhealthy-container">  <h1>Unhealthy</h1>  </div>');
+
+  const candiesImage = new ImageElementWidget(50, 400, 110, 110, 'assets/example-health/candies.png');
+  $('#app').append(candiesImage.domElem);
+  const appleImage = new ImageElementWidget(250, 400, 110, 110, 'assets/example-health/apple.png');
+  $('#app').append(appleImage.domElem);
+  const bananaImage = new ImageElementWidget(450, 400, 110, 110, 'assets/example-health/banana.png');
+  $('#app').append(bananaImage.domElem);
+  const chipsImage = new ImageElementWidget(650, 400, 110, 110, 'assets/example-health/chips.png');
+  $('#app').append(chipsImage.domElem);
+  const broccoliImage = new ImageElementWidget(850, 400, 110, 110, 'assets/example-health/broccoli.png');
+  $('#app').append(broccoliImage.domElem);
+  const tomatoImage = new ImageElementWidget(1050, 400, 110, 110, 'assets/example-health/tomato.png');
+  $('#app').append(tomatoImage.domElem);
+  const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+  $('#app').append(imageWidget.domElem);
+
+}// buildHealth()
+
+
+function buildDevelopment() {
+  $('#app').empty();
+
+  const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+  const videoWidget = new VideoElementWidget(100, 100, 250, 333, 'http://techslides.com/demos/sample-videos/small.mp4');
+  $('#app').append(imageWidget.domElem);
+  $('#app').append(videoWidget.domElem);
+}// buildDevelopment()
+
 export function buildMenu() {
   $('#app').append('<button id="development"> Development </button> </br>');
   $('#app').append('<button id="health"> Health (using ImageElementWidget) </button></br>');
@@ -22,43 +56,6 @@ export function buildMenu() {
     buildHealth();
   });
   $('#puzzle').on('click', () => {
-    console.log('puzzle clicked');
+    // console.log('puzzle clicked');
   });
 }// buildMenu()
-
-function buildDevelopment() {
-  $('#app').empty();
-
-  const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
-  const videoWidget = new VideoElementWidget(100, 100, 250, 333, 'http://techslides.com/demos/sample-videos/small.mp4');
-  $('#app').append(imageWidget.domElem);
-  $('#app').append(videoWidget.domElem);
-}// buildDevelopment()
-
- function buildHealth() {
-   $('#app').empty();
-
-   $('#app').append('<div id="healthy-container">  <h1>Healthy</h1>  </div>');
-   $('#app').append('<div id="unhealthy-container">  <h1>Unhealthy</h1>  </div>');
-
-   const candiesImage = new ImageElementWidget(50, 400, 110, 110, 'assets/example-health/candies.png');
-   $('#app').append(candiesImage.domElem);
-   const appleImage = new ImageElementWidget(250, 400, 110, 110, 'assets/example-health/apple.png');
-   $('#app').append(appleImage.domElem);
-   const bananaImage = new ImageElementWidget(450, 400, 110, 110, 'assets/example-health/banana.png');
-   $('#app').append(bananaImage.domElem);
-   const chipsImage = new ImageElementWidget(650, 400, 110, 110, 'assets/example-health/chips.png');
-   $('#app').append(chipsImage.domElem);
-   const broccoliImage = new ImageElementWidget(850, 400, 110, 110, 'assets/example-health/broccoli.png');
-   $('#app').append(broccoliImage.domElem);
-   const tomatoImage = new ImageElementWidget(1050, 400, 110, 110, 'assets/example-health/tomato.png');
-   $('#app').append(tomatoImage.domElem);
-   /*const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
-   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
-   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
-   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
-   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
-   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');*/
-
-
- }// buildHealth()
