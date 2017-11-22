d80e07e407a5481f38d2a3ea67a65565d7f483e2
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-11-22 14:14:24 +0100

    Displaying pictures in example 1

--- src/menu.js
+++ src/menu.js
@@ -11,14 +11,14 @@ import VideoElementWidget from 'tuiomanager/widgets/ElementWidget/VideoElementWi
 
 export function buildMenu() {
   $('#app').append('<button id="development"> Development </button> </br>');
-  $('#app').append('<button id="fruits"> Fruits and vegetables (using ImageElementWidget) </button></br>');
+  $('#app').append('<button id="health"> Health (using ImageElementWidget) </button></br>');
   $('#app').append('<button id="puzzle"> Puzzle (using ImageElementWidget) </button></br>');
 
 
   $('#development').on('click', () => {
     buildDevelopment();
   });
-  $('#fruits').on('click', () => {
+  $('#health').on('click', () => {
     buildHealth();
   });
   $('#puzzle').on('click', () => {
@@ -40,5 +40,21 @@ function buildDevelopment() {
 
    $('#app').append('<div id="healthy-container">  <h1>Healthy</h1>  </div>');
    $('#app').append('<div id="unhealthy-container">  <h1>Unhealthy</h1>  </div>');
+
+   const appleImage = new ImageElementWidget(50, 400, 150, 150, 'assets/example-health/apple.jpg');
+   $('#app').append(appleImage.domElem);
+   const bananaImage = new ImageElementWidget(250, 400, 150, 150, 'assets/example-health/banana.jpg');
+   $('#app').append(bananaImage.domElem);
+   const broccoliImage = new ImageElementWidget(450, 400, 150, 150, 'assets/example-health/broccoli.jpg');
+   $('#app').append(broccoliImage.domElem);
+   const tomatoImage = new ImageElementWidget(650, 400, 150, 150, 'assets/example-health/tomato.jpg');
+   $('#app').append(tomatoImage.domElem);
+   /*const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');*/
+
 
  }// buildHealth()
