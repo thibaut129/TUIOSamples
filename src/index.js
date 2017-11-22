e7ef4389badc42ee5d6a442027c6d16f41597e11
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-11-22 09:32:49 +0100

    Showing the menu for the examples

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import $ from 'jquery/dist/jquery.min';
 // Import TUIOManager
 import TUIOManager from 'tuiomanager/core/TUIOManager';
 
+import {buildMenu} from './menu';
+
+
 // Import ImageWidget
 import ImageElementWidget from 'tuiomanager/widgets/ElementWidget/ImageElementWidget/ImageElementWidget';
 import VideoElementWidget from 'tuiomanager/widgets/ElementWidget/VideoElementWidget/VideoElementWidget';
@@ -21,11 +24,19 @@ tuioManager.start();
 /** App Code **/
 
 const buildApp = () => {
-  const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
+  /*const imageWidget = new ImageElementWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
   const videoWidget = new VideoElementWidget(100, 100, 250, 333, 'http://techslides.com/demos/sample-videos/small.mp4');
   $('#app').append(imageWidget.domElem);
-  $('#app').append(videoWidget.domElem);
+  $('#app').append(videoWidget.domElem);*/
+
+  $('#app').append('<h1> TUIO Showcase </h1>');
+  buildMenu();
+
+
+
 };
+
+
 
 $(window).ready(() => {
   buildApp();
