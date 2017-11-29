583c0bd2a5b42436c9405bd73a31886420d6b68f
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-11-29 11:10:46 +0100

    Global array storing the widgets

--- src/menu.js
+++ src/menu.js
@@ -10,6 +10,22 @@ import ImageElementWidget from 'tuiomanager/widgets/ElementWidget/ImageElementWi
 import VideoElementWidget from 'tuiomanager/widgets/ElementWidget/VideoElementWidget/VideoElementWidget';
 import CircularMenu from 'tuiomanager/widgets/CircularMenu/CircularMenu';
 
+let widgets = [];
+
+function AddWidgetToScreen(widget) {
+  $('#example-container').append(widget.domElem);
+  widgets.push(widget);
+}//AddWidgetToScreen()
+
+function RemoveWidgets() {
+  $('#example-container').empty();
+  for (let i = 0; i < widgets.length; i += 1) {
+    widgets[i].deleteWidget();
+  }
+  widgets=[];
+}
+
+
 
 function getRand(min, max) {
   return Math.floor(Math.random() * ((max - min) + 1)) + min;
@@ -18,7 +34,7 @@ function getRand(min, max) {
 function buildBackButton() {
   $('#example-container').append('<button id="back-button">Back</button>');
   $('#back-button').on('click', () => {
-    $('#example-container').empty();
+    RemoveWidgets();
     buildMenu();
   });
 }//  displayMenu()
@@ -27,12 +43,14 @@ function buildDevelopment() {
   $('#example-container').empty();
   buildBackButton();
   const imageWidget = new ImageElementWidget(0, 0, 250, 333, 0, 2, 'assets/IMG_20150304_201145.jpg', 'B3', 'C9', '38');
+  AddWidgetToScreen(imageWidget);
   const videoWidget = new VideoElementWidget(100, 100, 250, 140, 0, 1, 'assets/video/video.mp4', 'B3', 'C9', '38', '3');
-  $('#example-container').append(imageWidget.domElem);
-  $('#example-container').append(videoWidget.domElem);
+  AddWidgetToScreen(videoWidget);
+
 
   const menuWidgetDev = new CircularMenu('6D', 8);
-  $('#app').append(menuWidgetDev.domElem);
+  //$('#app').append(menuWidgetDev.domElem);
+  AddWidgetToScreen(menuWidgetDev);
   menuWidgetDev.addMenuItemText('Facile', '#F00', '', () => {
     console.log('Facile Texte');
   });
@@ -70,17 +88,17 @@ function buildHealth() {
   $('.title-bottom').css('marginTop', `${height}px`);
 
   const candiesImage = new ImageElementWidget(100, 150, 110, 110, 0, 1, 'assets/example-health/candies.png', 'B3', 'C9', '38');
-  $('#example-container').append(candiesImage.domElem);
+  AddWidgetToScreen(candiesImage);
   const appleImage = new ImageElementWidget(850, 20, 110, 110, 0, 1, 'assets/example-health/apple.png', 'B3', 'C9', '38');
-  $('#example-container').append(appleImage.domElem);
+  AddWidgetToScreen(appleImage);
   const bananaImage = new ImageElementWidget(1700, 500, 110, 110, 0, 1, 'assets/example-health/banana.png', 'B3', 'C9', '38');
-  $('#example-container').append(bananaImage.domElem);
+  AddWidgetToScreen(bananaImage);
   const chipsImage = new ImageElementWidget(1500, 20, 110, 110, 0, 1, 'assets/example-health/chips.png', 'B3', 'C9', '38');
-  $('#example-container').append(chipsImage.domElem);
+  AddWidgetToScreen(chipsImage);
   const broccoliImage = new ImageElementWidget(200, 700, 110, 110, 0, 1, 'assets/example-health/broccoli.png', 'B3', 'C9', '38');
-  $('#example-container').append(broccoliImage.domElem);
+  AddWidgetToScreen(broccoliImage);
   const tomatoImage = new ImageElementWidget(1000, 850, 110, 110, 90, 1, 'assets/example-health/tomato.png', 'B3', 'C9', '38');
-  $('#example-container').append(tomatoImage.domElem);
+  AddWidgetToScreen(tomatoImage);
 }// buildHealth()
 
 function SpawnRotation(difficulty) {
@@ -120,7 +138,7 @@ function buildPuzzle(difficulty) {
   pieces.push(puz8);
 
   for (let i = 0; i < pieces.length; i += 1) {
-    $('#example-container').append(pieces[i].domElem);
+    AddWidgetToScreen(pieces[i]);
 
     if (difficulty === 'easy') {
       pieces[i].canZoom(false, false);
@@ -133,7 +151,8 @@ function buildPuzzle(difficulty) {
     }
   }
   const menuWidget = new CircularMenu('6D', 3);
-  $('#app').append(menuWidget.domElem);
+  //$('#app').append(menuWidget.domElem);
+  AddWidgetToScreen(menuWidget);
   menuWidget.addMenuItemText('Facile', '#FFF', '#2E7D32', () => {
     menuWidget.deleteWidget();
     buildPuzzle('easy');
@@ -157,15 +176,15 @@ function buildMusic() {
   $('#music-container').append('<div class="music-subcontainer"><img src="assets/example-music/saxophone.png" ><div class="music-target"></div> </div>');
 
   const fluteVid = new VideoElementWidget(250, 800, 150, 84, SpawnRotation('medium'), 1, 'assets/example-music/flute.mp4', 'B3', 'C9', '38', '3', '');
-  $('#example-container').append(fluteVid.domElem);
+  AddWidgetToScreen(fluteVid);
   const pianoVid = new VideoElementWidget(450, 100, 150, 84, SpawnRotation('medium'), 1, 'assets/example-music/piano.mp4', 'B3', 'C9', '38', '3', '');
-  $('#example-container').append(pianoVid.domElem);
+  AddWidgetToScreen(fluteVid);
   const drumsVid = new VideoElementWidget(650, 800, 150, 84, SpawnRotation('medium'), 1, 'assets/example-music/drums.mp4', 'B3', 'C9', '38', '3', '');
-  $('#example-container').append(drumsVid.domElem);
+  AddWidgetToScreen(fluteVid);
   const guitarVid = new VideoElementWidget(850, 100, 150, 84, SpawnRotation('medium'), 1, 'assets/example-music/guitar.mp4', 'B3', 'C9', '38', '3', '');
-  $('#example-container').append(guitarVid.domElem);
+  AddWidgetToScreen(fluteVid);
   const saxophoneVid = new VideoElementWidget(1050, 800, 150, 84, SpawnRotation('medium'), 1, 'assets/example-music/saxophone.mp4', 'B3', 'C9', '38', '3', '');
-  $('#example-container').append(saxophoneVid.domElem);
+  AddWidgetToScreen(fluteVid);
 } // buildMusic()
 
 
